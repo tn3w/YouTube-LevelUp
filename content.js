6e3bd693b326d109f75916b42bd0292f951c9f73
tn3w <tn3w@protonmail.com>
2026-04-04 00:22:32 +0200

    fix: update version to 1.3.9.5 and remove redundant Shorts link from content scripts, possible black box fix https://github.com/tn3w/YouTube-LevelUp/issues/9

--- content.js
+++ content.js
@@ -1021,7 +1021,6 @@
             ytm-rich-section-renderer:has([href^="/shorts/"]),
             grid-shelf-view-model:has([href^="/shorts/"]),
             ytd-rich-grid-group:has([href^="/shorts/"]),
-            [href^="/shorts/"],
             ytd-reel-item-renderer,
             ytm-reel-item-renderer,
             ytm-shorts-lockup-view-model,
